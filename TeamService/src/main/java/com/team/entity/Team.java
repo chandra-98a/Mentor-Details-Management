a887b2a10de9257b49dc78c00ea0f6dc03d46dae
chandra-98a <chandrakalaa2022@gmail.com>
2023-12-20 11:27:10 +0530

    TeamJPAEntity Class

---
+++ TeamService/src/main/java/com/team/entity/Team.java
@@ -0,0 +1,53 @@
+package com.team.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="Teams")
+public class Team {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	private String teamName;
+	private String teamDescription;
+	private String teamCode;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getTeamName() {
+		return teamName;
+	}
+	public void setTeamName(String teamName) {
+		this.teamName = teamName;
+	}
+	public String getTeamDescription() {
+		return teamDescription;
+	}
+	public void setTeamDescription(String teamDescription) {
+		this.teamDescription = teamDescription;
+	}
+	public String getTeamCode() {
+		return teamCode;
+	}
+	public void setTeamCode(String teamCode) {
+		this.teamCode = teamCode;
+	}
+	
+	
+
+}
