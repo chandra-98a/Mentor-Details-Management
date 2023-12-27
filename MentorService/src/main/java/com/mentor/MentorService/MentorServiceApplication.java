9957c362b8f30e60448a7ec6997ebd45c26b9adc
chandra-98a <chandrakalaa2022@gmail.com>
2023-12-27 16:24:54 +0530

    Register Mentor-Service with eureka client

--- MentorService/src/main/java/com/mentor/MentorService/MentorServiceApplication.java
+++ MentorService/src/main/java/com/mentor/MentorService/MentorServiceApplication.java
@@ -2,11 +2,12 @@ package com.mentor.MentorService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
-
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableFeignClients
 public class MentorServiceApplication {
